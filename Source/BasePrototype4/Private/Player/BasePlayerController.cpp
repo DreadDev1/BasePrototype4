6033ef0b98c2a22aa80fd0d44e6c8c0f6da771d9
DreadDev1 <79171586+DreadDev1@users.noreply.github.com>
2025-08-08 00:34:02 -0400

    Setup InputMappingContext and Input Actions

--- Source/BasePrototype4/Private/Player/BasePlayerController.cpp
+++ Source/BasePrototype4/Private/Player/BasePlayerController.cpp
@@ -3,3 +3,52 @@
 
 #include "Player/BasePlayerController.h"
 
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+
+ABasePlayerController::ABasePlayerController()
+{
+}
+
+void ABasePlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	if (IsValid(Subsystem))
+	{
+		for (UInputMappingContext* CurrentContext : DefaultIMCs) { Subsystem->AddMappingContext(CurrentContext, 0); }
+	}
+
+}
+
+void ABasePlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+	
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ABasePlayerController::Move);
+	EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ABasePlayerController::Look);
+}
+
+void ABasePlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
+	const FRotator Rotation = GetControlRotation();
+	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	if (APawn* ControlledPawn = GetPawn<APawn>())
+	{
+		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
+		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+	}
+}
+
+void ABasePlayerController::Look(const FInputActionValue& InputActionValue)
+{
+	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
+	AddYawInput(InputAxisVector.X);
+	AddPitchInput(InputAxisVector.Y);
+}
--- Source/BasePrototype4/Public/Player/BasePlayerController.h
+++ Source/BasePrototype4/Public/Player/BasePlayerController.h
@@ -6,12 +6,31 @@
 #include "GameFramework/PlayerController.h"
 #include "BasePlayerController.generated.h"
 
-/**
- * 
- */
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
+
 UCLASS()
 class BASEPROTOTYPE4_API ABasePlayerController : public APlayerController
 {
 	GENERATED_BODY()
+public:
+	ABasePlayerController();
 	
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+	
+#pragma region Input Mappings - Actions
+	UPROPERTY(EditDefaultsOnly, Category = "BasePrototype|InputMappingContext")
+	TArray<TObjectPtr<UInputMappingContext>> DefaultIMCs;
+   	
+	void Move(const FInputActionValue& InputActionValue);
+	UPROPERTY(EditDefaultsOnly, Category= "BasePrototype|InputActions|Movement")
+	TObjectPtr<UInputAction> MoveAction;
+    
+	void Look(const FInputActionValue& InputActionValue);
+	UPROPERTY(EditDefaultsOnly, Category=  "BasePrototype|InputActions|Movement")
+	TObjectPtr<UInputAction> LookAction;
+#pragma endregion
 };
